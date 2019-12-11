cd0cc7c8f4840dd8bc4c1993b4f6454c23c3ae54
adri51 <adriibm91@gmail.com>
2019-12-11 17:36:25 +0100

    dbjs

--- db.js
+++ db.js
@@ -6,19 +6,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE
-<<<<<<< HEAD
 
 });
-
-/* const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'adoptadog'
-}) */
-=======
-});
->>>>>>> 03c1df678c36a8aad66008c787468c11b5230df1
 
 global.db = pool;
