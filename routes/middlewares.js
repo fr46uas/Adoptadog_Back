e159792d8a6701403064cd5bb9c72c750546c335
FR46UAS <sergio.fraguas@gmail.com>
2020-03-10 14:55:40 +0100

    .

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,27 +1,27 @@
-// const jwt = require('jwt-simple');
-// const moment = require('moment');
+const jwt = require('jwt-simple');
+const moment = require('moment');
 
-// const checkToken = (req, res, next) => {
-//     //COMPRUEBO SI LA CABECERA ESTA INCLUIDA EN LA PETICION
-//     if (!req.headers['user-token']) {
-//         return res.json({ error: 'Tienes que incluir la cabecera user-token' })
-//     }
+const checkToken = (req, res, next) => {
+    //     //COMPRUEBO SI LA CABECERA ESTA INCLUIDA EN LA PETICION
+    if (!req.headers['user-token']) {
+        return res.json({ error: 'Tienes que incluir la cabecera user-token' })
+    }
 
-//     const token = req.headers['user-token'];
-//     let payload = null;
-//     try {
-//         payload = jwt.decode(token, process.env.TOKEN_KEY);
-//     } catch (err) {
-//         return res.json({ error: 'El token es invalido' })
-//     };
+    const token = req.headers['user-token'];
+    let payload = null;
+    try {
+        payload = jwt.decode(token, process.env.TOKEN_KEY);
+    } catch (err) {
+        return res.json({ error: 'El token es invalido' })
+    };
 
-//     if (moment().unix() > payload.expiresAt) {
-//         return res.json({ error: 'El token esta caducado. Pide otro' })
-//     }
+    if (moment().unix() > payload.expiresAt) {
+        return res.json({ error: 'El token esta caducado. Pide otro' })
+    }
 
-//     next();
-// };
+    next();
+};
 
-// module.exports = {
-//     checkToken: checkToken
-// }
+module.exports = {
+    checkToken: checkToken
+}
